bc87ff3d10f5548802c65cbd3aab4e4426a22bc3
J.P. Barela <barela@jpbarela.com>
2016-07-18 08:27:21 -0600

    Update heroku configs.

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 class Tunr < Sinatra::Base
-  configure do
-	Mongoid.load! "#{File.dirname(__FILE__)}/config/mongoid.yml"
-  end
+  p "Rack Environment #{ENV[]}"
+
+ #  configure do
+	# Mongoid.load! "#{File.dirname(__FILE__)}/config/mongoid.yml"
+ #  end
 
 
   # General route actions
